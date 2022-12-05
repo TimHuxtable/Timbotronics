cd70ca15a144d11e5a6afe1bde4067d4934d90cc
Timothy Huxtable <huxtable.tim30@gmail.com>
2022-12-05 15:59:43 -0600

    Testing mac

--- mysql_helper.php
+++ mysql_helper.php
@@ -15,7 +15,6 @@ class mysql_helper
             $password = "WhyAr3Y0uGay?";
         }
 		$database = "TimboTronics";
-//hello
 		$this->connection = new mysqli($servername, $username, $password, $database);
 		if ($this->connection->connect_error) {
 			die("Connection failed: " . $this->connection->connect_error);
